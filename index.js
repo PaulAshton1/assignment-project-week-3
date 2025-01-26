51c68b959defda7cbcbca657a8048d564b14a899
PaulAshton1 <ashtonvilla42@gmail.com>
2025-01-26 20:51:25 +0300

    adds new file

---
+++ index.js
@@ -0,0 +1,41 @@
+    //Male Akane names
+// Using object literal notation
+// let dictionary = {'male':{}};
+// dictionary['male']["Sunday"] = "Kwasi";
+// dictionary["Monday"] = "Kwadwo";
+// dictionary["Tuesday"] = "Kwabena";
+// dictionary["Wednesday"] = "Kwaku";
+// dictionary["Thurday"] = "Yaw";
+// dictionary["Friday"] = "Kofi";
+// dictionary["Saturday"] = "Kwame";
+// console.log(dictionary);
+
+const gh_culture ={
+    'Male' : { "Sunday" : "Kwasi",
+                "Monday" : "Kwadwo",
+                "Tuesday" : "Kwabena",
+                "Wednesday" : "Kwaku",
+                "Thursday" : "Yaw",
+                "Friday" : "Kofi",
+                "Saturday" : "Kwame",      
+
+    },
+    'Female' : { "Sunday" : "Akosua",
+                    "Monday" : "Adwoa",
+                    "Tueday" : "Abenaa",
+                    "Wednesday" : "Akua",
+                    "Thursday" : "Yaa",
+                    "Friday" : "Afua",
+                    "Saturday" : "Ama",
+
+    }
+
+}
+console.log(gh_culture)
+
+//Current day and time
+
+const date = new Date();
+console.log(date);
+
+//
